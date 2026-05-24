9a579e3d8e3503ed88c0f9282cac75006e6a9cea
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-05-25 05:05:24 +0530

    Added localStorage for StopWatch
    When the stopwatch is OFF -> save stopwatchSeconds
    When the stopwatch is ON -> save the time at which the timer started

--- src/pages/StopwatchPage.jsx
+++ src/pages/StopwatchPage.jsx
@@ -1,11 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Stopwatch from "../components/Stopwatch";
 import StopwatchOptions from "../components/StopwatchOptions";
+import { IS_STOPWATCH_ON_KEY, STOPWATCH_SECONDS_KEY, STOPWATCH_START_TIME_KEY } from '../utils/keys';
+import cleanGetItem from '../utils/cleanGetItem';
 
 function StopwatchPage() {
-    const CURRENT_PAGE = "stopwatch";
-    const [isStopwatchOn, setIsStopwatchOn] = useState(false);
-    const [stopwatchSeconds, setStopwatchSeconds] = useState(0);
+    const [isStopwatchOn, setIsStopwatchOn] = useState(cleanGetItem(IS_STOPWATCH_ON_KEY, false));
+    const [stopwatchSeconds, setStopwatchSeconds] = useState(cleanGetItem(STOPWATCH_SECONDS_KEY, 0));
+    const stopwatchStartTime = cleanGetItem(STOPWATCH_START_TIME_KEY, new Date());
+
+    useEffect(() => {
+        if (isStopwatchOn) {
+            const milliSecondsPassed = new Date() - new Date(stopwatchStartTime);
+            const secondsPassed = Math.floor(milliSecondsPassed / 1000);
+            setStopwatchSeconds(secondsPassed);
+        }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem(IS_STOPWATCH_ON_KEY, isStopwatchOn);
+        if (isStopwatchOn && stopwatchSeconds === 0) {
+            localStorage.setItem(STOPWATCH_START_TIME_KEY, JSON.stringify(new Date()));
+        }
+    }, [isStopwatchOn, stopwatchSeconds]);
+
+    useEffect(() => {
+        localStorage.setItem(STOPWATCH_SECONDS_KEY, stopwatchSeconds);
+    }, [stopwatchSeconds])
 
     return (
         <>

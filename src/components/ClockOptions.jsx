4bdf2134ac27109d536936e51304a71b3e41c538
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-05-24 21:57:55 +0530

    Removed debugging console.log and added input through label

--- src/components/ClockOptions.jsx
+++ src/components/ClockOptions.jsx
@@ -1,16 +1,14 @@
 import './ClockOptions.css';
 
 function ClockOptions({ use24Hours, setUse24Hours }) {
-
     function toggleUse24Hours() {
         setUse24Hours(!use24Hours);
-        console.log("123");
     }
 
     return (
         <div className="clockOptionsContainer">
-            <input type="checkbox" onInput={toggleUse24Hours} />
-            <p>Use 24 Hour Clock</p>
+            <input type="checkbox" checked={use24Hours} onChange={toggleUse24Hours} />
+            <p className="Use24HoursLabel" onClick={toggleUse24Hours}>Use 24-Hour Clock</p>
         </div>
     );
 }

0139a637ebb3ed9f6573e9a18bb4e3cad1963c9b
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-05-25 04:08:10 +0530

    Implemented localStorage for use24Hours state

--- src/pages/ClockPage.jsx
+++ src/pages/ClockPage.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Clock from '../components/Clock';
 import ClockOptions from '../components/ClockOptions';
 import NavigationPane from '../components/NavigationPane';
+import { USE_24_HOURS_KEY } from '../utils/keys';
+import cleanGetItem from '../utils/cleanGetItem';
 
 function ClockPage() {
     const CURRENT_PAGE = "clock";
-    const [use24Hours, setUse24Hours] = useState(false);
+    const [use24Hours, setUse24Hours] = useState(cleanGetItem(USE_24_HOURS_KEY, false));
     const [popLabel, setPopLabel] = useState(false);
+
+    useEffect(() => {
+        localStorage.setItem(USE_24_HOURS_KEY, use24Hours);
+    }, [use24Hours]);
 
     return (
         <>

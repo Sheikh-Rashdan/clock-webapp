65eb10e9071cc9935713ba253870180bf6453f1a
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-05-25 00:37:16 +0530

    Created outline for StopwatchOptions

---
+++ src/components/StopwatchOptions.jsx
@@ -0,0 +1,12 @@
+import "./Containers.css";
+import "./StopwatchOptions.css";
+
+function StopwatchOptions() {
+    return (
+        <div className="optionsContainer">
+
+        </div>
+    );
+}
+
+export default StopwatchOptions;
--- src/pages/StopwatchPage.jsx
+++ src/pages/StopwatchPage.jsx
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import Stopwatch from "../components/Stopwatch";
+import StopwatchOptions from "../components/StopwatchOptions";
 import NavigationPane from '../components/NavigationPane';
 
 function StopwatchPage() {
     const CURRENT_PAGE = "stopwatch";
+    const [isStopwatchOn, setIsStopwatchOn] = useState(false);
+    const [stopwatchSeconds, setStopwatchSeconds] = useState(0);
 
     return (
         <>
-            <Stopwatch />
+            <Stopwatch {...{ isStopwatchOn, stopwatchSeconds, setStopwatchSeconds }} />
+            <StopwatchOptions />
             <NavigationPane highlight={CURRENT_PAGE} />
         </>
     );

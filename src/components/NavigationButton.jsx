2ddc9b7e190f2bcb8eda586bf114de35b5fa9fce
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-05-25 04:28:50 +0530

    Modified hover style for NavigationButton and reworked highlight logic

--- src/components/NavigationButton.jsx
+++ src/components/NavigationButton.jsx
@@ -1,16 +1,20 @@
 import { NavLink } from 'react-router';
+import { useLocation } from 'react-router';
 
 import './NavigationButton.css';
 
-function NavigationButton({ path, icon, highlight }) {
+function NavigationButton({ path, icon }) {
+    const location = useLocation();
+    let locationPath = location.pathname.split("/");
+    locationPath = locationPath[locationPath.length - 1];
 
     function toTitleCase(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
     return (
-        <NavLink to={`/${path}`}>
-            <div className={`navigationButton ${highlight === path ? "highlight" : ""}`}>
+        <NavLink to={`${path}`}>
+            <div className={`navigationButton ${locationPath === path ? "highlight" : ""}`}>
                 <img src={icon} />
                 {toTitleCase(path)}
             </div>

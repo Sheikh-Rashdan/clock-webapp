6241bb7c2ab73d8f820e9da111a4e6e1a191fcaf
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-05-25 17:45:03 +0530

    Added alarmData to localStorage

--- src/pages/AlarmPage.jsx
+++ src/pages/AlarmPage.jsx
@@ -2,12 +2,16 @@ import { useState, useEffect } from "react";
 import Alarm from "../components/Alarm";
 import alarmSound from "../assets/alarm/Alarm.mp3";
 import AlarmDisplay from "../components/AlarmDisplay";
+import { ALARM_DATA_KEY } from "../utils/keys";
+import cleanGetItem from "../utils/cleanGetItem";
 
 function AlarmPage() {
-    const [alarmData, setAlarmData] = useState([]);
+    const [alarmData, setAlarmData] = useState(cleanGetItem(ALARM_DATA_KEY, []));
     const alarmAudio = new Audio(alarmSound);
 
     useEffect(() => {
+        localStorage.setItem(ALARM_DATA_KEY, JSON.stringify(alarmData));
+
         const interval = setInterval(() => {
             const now = new Date();
             const index = alarmData.findIndex((data) => data.hours === now.getHours() && data.minutes === now.getMinutes());
@@ -18,7 +22,7 @@ function AlarmPage() {
                 newAlarmData.splice(index, 1);
                 setAlarmData(newAlarmData);
             }
-        }, 1000);
+        }, 15000);
 
         return () => clearInterval(interval);
     }, [alarmData])

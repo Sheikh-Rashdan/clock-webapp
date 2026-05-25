55c9a1c4cd0797f0d592f3311d11d4ef197d2ff3
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-05-25 05:50:24 +0530

    Undid changes related to localStorage of StopWatch in ON stage due to complexity that reached beyond the scope of this project

--- src/pages/StopwatchPage.jsx
+++ src/pages/StopwatchPage.jsx
@@ -1,29 +1,12 @@
 import { useState, useEffect } from 'react';
 import Stopwatch from "../components/Stopwatch";
 import StopwatchOptions from "../components/StopwatchOptions";
-import { IS_STOPWATCH_ON_KEY, STOPWATCH_SECONDS_KEY, STOPWATCH_START_TIME_KEY } from '../utils/keys';
+import { STOPWATCH_SECONDS_KEY } from '../utils/keys';
 import cleanGetItem from '../utils/cleanGetItem';
 
 function StopwatchPage() {
-    const stopwatchStartTime = cleanGetItem(STOPWATCH_START_TIME_KEY, new Date());
-    const [isStopwatchOn, setIsStopwatchOn] = useState(cleanGetItem(IS_STOPWATCH_ON_KEY, false));
-    const [stopwatchSeconds, setStopwatchSeconds] = useState(() => {
-        const storedSeconds = cleanGetItem(STOPWATCH_SECONDS_KEY, 0);
-
-        if (isStopwatchOn) {
-            const milliSecondsPassed = new Date() - new Date(stopwatchStartTime);
-            return Math.floor(milliSecondsPassed / 1000);
-        }
-
-        return storedSeconds;
-    });
-
-    useEffect(() => {
-        localStorage.setItem(IS_STOPWATCH_ON_KEY, isStopwatchOn);
-        if (isStopwatchOn && stopwatchSeconds === 0) {
-            localStorage.setItem(STOPWATCH_START_TIME_KEY, JSON.stringify(new Date()));
-        }
-    }, [isStopwatchOn, stopwatchSeconds]);
+    const [isStopwatchOn, setIsStopwatchOn] = useState(false);
+    const [stopwatchSeconds, setStopwatchSeconds] = useState(cleanGetItem(STOPWATCH_SECONDS_KEY, 0))
 
     useEffect(() => {
         localStorage.setItem(STOPWATCH_SECONDS_KEY, stopwatchSeconds);

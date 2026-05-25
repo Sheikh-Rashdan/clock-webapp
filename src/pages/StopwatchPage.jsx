522988f5a5521ef6a8532bbb80792f0f0c592ba0
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-05-25 05:41:58 +0530

    Implemented initializer function for stopwatchSeconds instead of run-once setEffect

--- src/pages/StopwatchPage.jsx
+++ src/pages/StopwatchPage.jsx
@@ -5,21 +5,18 @@ import { IS_STOPWATCH_ON_KEY, STOPWATCH_SECONDS_KEY, STOPWATCH_START_TIME_KEY } 
 import cleanGetItem from '../utils/cleanGetItem';
 
 function StopwatchPage() {
-    const [isStopwatchOn, setIsStopwatchOn] = useState(cleanGetItem(IS_STOPWATCH_ON_KEY, false));
-    const [stopwatchSeconds, setStopwatchSeconds] = useState(cleanGetItem(STOPWATCH_SECONDS_KEY, 0));
     const stopwatchStartTime = cleanGetItem(STOPWATCH_START_TIME_KEY, new Date());
+    const [isStopwatchOn, setIsStopwatchOn] = useState(cleanGetItem(IS_STOPWATCH_ON_KEY, false));
+    const [stopwatchSeconds, setStopwatchSeconds] = useState(() => {
+        const storedSeconds = cleanGetItem(STOPWATCH_SECONDS_KEY, 0);
 
-    function setStopwatchSecondByLocalStorage() {
-        const milliSecondsPassed = new Date() - new Date(stopwatchStartTime);
-        const secondsPassed = Math.floor(milliSecondsPassed / 1000);
-        setStopwatchSeconds(secondsPassed);
-    }
-
-    useEffect(() => {
         if (isStopwatchOn) {
-            setStopwatchSecondByLocalStorage();
+            const milliSecondsPassed = new Date() - new Date(stopwatchStartTime);
+            return Math.floor(milliSecondsPassed / 1000);
         }
-    }, [])
+
+        return storedSeconds;
+    });
 
     useEffect(() => {
         localStorage.setItem(IS_STOPWATCH_ON_KEY, isStopwatchOn);

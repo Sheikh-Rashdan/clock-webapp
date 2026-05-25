3abb48a97735d9d4f061f5469575eb6a886e6f9f
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-05-25 06:01:42 +0530

    Added keyboard shortcuts to Stopwatch
    Space - Pause/Play
    R - Reset

--- src/pages/StopwatchPage.jsx
+++ src/pages/StopwatchPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import Stopwatch from "../components/Stopwatch";
 import StopwatchOptions from "../components/StopwatchOptions";
 import { STOPWATCH_SECONDS_KEY } from '../utils/keys';
@@ -7,6 +7,25 @@ import cleanGetItem from '../utils/cleanGetItem';
 function StopwatchPage() {
     const [isStopwatchOn, setIsStopwatchOn] = useState(false);
     const [stopwatchSeconds, setStopwatchSeconds] = useState(cleanGetItem(STOPWATCH_SECONDS_KEY, 0))
+
+    const handleKeyDowns = useCallback((e) => {
+        const { key } = e;
+
+        if (key === " ") {
+            setIsStopwatchOn(!isStopwatchOn);
+        }
+        else if (key === "r") {
+            setStopwatchSeconds(0);
+        }
+    }, [isStopwatchOn]);
+
+    useEffect(() => {
+        addEventListener('keydown', handleKeyDowns);
+
+        return () => {
+            removeEventListener('keydown', handleKeyDowns);
+        }
+    }, [handleKeyDowns]);
 
     useEffect(() => {
         localStorage.setItem(STOPWATCH_SECONDS_KEY, stopwatchSeconds);

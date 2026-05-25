62aa3d4f3f05ae2d0e5a5f045c4d1cc485d8ea81
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-05-25 13:42:19 +0530

    Implemented feature to scroll lapTimeContainer to bottom when lapping

--- src/components/LapInfo.jsx
+++ src/components/LapInfo.jsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react';
 import zfill from "../utils/zfill";
 import { getFormattedStopwatchSeconds } from "../utils/formatted";
 
@@ -6,13 +7,21 @@ import "./Containers.css";
 import closeIcon from "../assets/stopwatch/close.png";
 
 function LapInfo({ lapData, setLapData }) {
+    const lapTimeContainerRef = useRef();
 
     function clearLapData() {
         setLapData([]);
     }
 
+    useEffect(() => {
+        const lapTimeContainer = lapTimeContainerRef.current;
+        if (lapTimeContainer !== undefined) {
+            lapTimeContainer.scrollTop = lapTimeContainer.scrollHeight;
+        }
+    }, [lapData]);
+
     return (
-        <div className="optionsContainer lapTimeContainer">
+        <div className="optionsContainer lapTimeContainer" ref={lapTimeContainerRef}>
             <div className="lapTimeLabelContainer">
                 <p className="lapTimeLabel">Lap Times</p>
                 <img src={closeIcon} onClick={clearLapData} />

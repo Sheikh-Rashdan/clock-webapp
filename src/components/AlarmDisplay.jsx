69938f5217b6730198874876109abaa340b4fc3b
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-05-25 17:37:20 +0530

    Fixed removing alarmData and implemented AlarmDisplay

--- src/components/AlarmDisplay.jsx
+++ src/components/AlarmDisplay.jsx
@@ -1,9 +1,44 @@
+import { useEffect, useRef } from "react";
+import zfill from "../utils/zfill";
+
 import "./AlarmDisplay.css";
 import "./Containers.css";
 
-function AlarmDisplay() {
+function AlarmDisplay({ alarmData, setAlarmData }) {
+    const containerRef = useRef();
+
+    useEffect(() => {
+        const container = containerRef.current;
+        if (container) {
+            container.scrollTop = container.scrollHeight;
+        }
+    }, [alarmData]);
+
+    function deleteAlarm(dataToDelete) {
+        const newAlarmData = [...alarmData];
+        const index = newAlarmData.findIndex(data => data.hours === dataToDelete.hours && data.minutes === dataToDelete.minutes);
+        if (index !== -1) {
+            newAlarmData.splice(index, 1);
+            setAlarmData(newAlarmData);
+        }
+    }
+
     return (
-        <div className="optionContainer">
+        <div className="optionsContainer column popThis" ref={containerRef}>
+            <p className="label">Alarms</p>
+            <div className="alarmDisplayContainer">
+                {alarmData.map(data => {
+                    let meridiem = "AM";
+                    let hours = data.hours;
+                    if (data.hours >= 12) {
+                        meridiem = "PM";
+                    }
+                    if (data.hours > 12) hours -= 12;
+                    else if (data.hours === 0) hours = 12;
+
+                    return <p key={`KEY_${data.hours}_${data.minutes}`} className="alarmCard" onClick={() => deleteAlarm(data)}>{zfill(hours)}:{zfill(data.minutes)} {meridiem}</p>
+                })}
+            </div>
         </div>
     );
 }
--- src/pages/AlarmPage.jsx
+++ src/pages/AlarmPage.jsx
@@ -1,21 +1,21 @@
 import { useState, useEffect } from "react";
 import Alarm from "../components/Alarm";
 import alarmSound from "../assets/alarm/Alarm.mp3";
+import AlarmDisplay from "../components/AlarmDisplay";
 
 function AlarmPage() {
     const [alarmData, setAlarmData] = useState([]);
     const alarmAudio = new Audio(alarmSound);
 
-    console.log(alarmData);
-
     useEffect(() => {
         const interval = setInterval(() => {
             const now = new Date();
             const index = alarmData.findIndex((data) => data.hours === now.getHours() && data.minutes === now.getMinutes());
+            console.log(index);
             if (index !== -1) {
                 alarmAudio.play();
                 const newAlarmData = [...alarmData];
-                newAlarmData.pop(index);
+                newAlarmData.splice(index, 1);
                 setAlarmData(newAlarmData);
             }
         }, 1000);
@@ -26,6 +26,7 @@ function AlarmPage() {
     return (
         <>
             <Alarm {...{ alarmData, setAlarmData }} />
+            {alarmData.length > 0 ? <AlarmDisplay {...{ alarmData, setAlarmData }} /> : ""}
         </>
     );
 }
